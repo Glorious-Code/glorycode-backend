78c141bfeb8ddf8bfa61b627e0faab5abc400e2a
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-12-29 17:42:33 +0100

    Update Forgot Password Page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { Head, useForm } from '@inertiajs/vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import {Head, Link, useForm} from '@inertiajs/vue3';
+import InputLabel from "@/Components/Form/InputLabel.vue";
+import InputText from "@/Components/Form/InputText.vue";
+import InputError from "@/Components/Form/InputError.vue";
+import Checkbox from "@/Components/Form/Checkbox.vue";
+import ToastSuccess from "@/Components/Toast/ToastSuccess.vue";
+import CardWithLogo from "@/Components/Card/CardWithLogo.vue";
+import ButtonPrimary from "@/Components/Button/ButtonPrimary.vue";
+import BlankPage from "@/Layouts/BlankPage.vue";
+import ModalSuccess from "@/Components/Modal/ModalSuccess.vue";
 
 defineProps({
     status: String,
@@ -23,39 +26,39 @@ const submit = () => {
 <template>
     <Head title="Forgot Password" />
 
-    <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
-
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
-        </div>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
-        </form>
-    </AuthenticationCard>
+    <BlankPage>
+        <section>
+            <ModalSuccess v-if="status" :message="status" trigger="successModal" />
+            <CardWithLogo>
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Forgot your password?
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" @submit.prevent="submit">
+                        <div>
+                            <InputLabel for="email" value="Your Email" />
+                            <InputText
+                                id="email"
+                                v-model="form.email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                required
+                                autofocus
+                                autocomplete="username"
+                                placeholder="john@glorycode.nl"
+                                :error="form.errors.email"
+                            />
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+                        <ButtonPrimary type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Email Password Reset Link
+                        </ButtonPrimary>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            I remember my password! <Link :href="route('login')" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</Link>
+                        </p>
+                    </form>
+                </div>
+            </CardWithLogo>
+        </section>
+    </BlankPage>
 </template>
