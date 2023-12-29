fe9456ab508799a59be0c51ae2a24161e0a931fd
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-12-29 14:23:54 +0100

    Use Amber Color

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -23,7 +23,7 @@ class FrontController extends Controller
             new FrontService('Assess Pro', 'Discover brilliance with Glorious AssessPro - the key to unlocking tech potential. Assess and uncover the brightest talents with rigorous technical evaluations. Data-driven decisions ensure you find the perfect fit for your team. Unravel tech brilliance - one assessment at a time.', '<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />'),
         ];
 
-        return Inertia::render('Welcome', [
+        return Inertia::render('Frontpage', [
             'services' => $services,
         ]);
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import colors from "tailwindcss/colors.js";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,7 +19,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: { "50": "#EBF5FF", "100": "#E1EFFE", "200": "#C3DDFD", "300": "#A4CAFE", "400": "#76A9FA", "500": "#3F83F8", "600": "#1C64F2", "700": "#1A56DB", "800": "#1E429F", "900": "#233876" }
+                primary: colors.amber
             }
         },
     },
